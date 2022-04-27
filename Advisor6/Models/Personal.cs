4a5356d87b0614883871f16f43dd88089a00acd4
mohammed.cos@outlook.com <mohammed.cos@outlook.com>
2022-04-27 14:03:48 +0300

    add personal model

---
+++ Advisor6/Models/Personal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Advisor6.Models
+{
+    public class Personal
+    {
+        [Key]
+        public string P_id { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string BirthDate { get; set; }
+        public string BornPlace { get; set; }
+        public string Nots { get; set; }
+        public string Image { get; set; }
+        public string PDF { get; set; }
+        
+
+
+    }
+}
