79e04ec29a4e66f0914035ed7ae7bdd01bf8d6e3
MohammedAGN <alimuhammed.ghazi@gmail.com>
2022-04-28 03:00:36 +0300

    Add-Administrative_Orders+DataFolder

---
+++ Advisor6/Models/Administrative_Orders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Advisor6.Models
+{
+    public class Administrative_Orders
+    {
+        [Key]
+        public string Admi_id { get; set; }
+        public int AdministrativeNo { get; set; }
+        public int OrderNo { get; set; }
+        public DateTime Administrative_Order_Date { get; set; }
+        public string Type_Of_Order { get; set; }
+        public string Subject { get; set; }
+        public string Summary { get; set; }
+        public string Nots { get; set; }
+       
+        public string Image { get; set; }
+        public string PDF { get; set; }
+    }
+}
