ee01da696ff8671b7d894f31c2b0691404775cbe
Ali Muhammed <alimuhammed.ghazi@gmail.com>
2022-06-09 16:42:19 +0300

    office - entry

--- Advisor6/Data/AppDbInitializer.cs
+++ Advisor6/Data/AppDbInitializer.cs
@@ -47,63 +47,63 @@ namespace Advisor6.Data
                     context.SaveChanges();
                 }
                 //Employment_info
-                if (!context.Employment_info.Any())
-                {
-                    context.Employment_info.AddRange(new List<Employment_info>()
-                    {
-                        new Employment_info()
-                        {
-                            FullName = "مستشار",
-                            MainDeptartment = null,
-                            SubDeptartment = null,
-                            Salary = null,
-                            Service_Continuation = Service_Continuation.continuous,
-                            Job_Title = null,
-                            Job_Status = null,
-                            EmploymentBook_No = null,
-                            EmploymentBook_Date = DateTime.Today,
-                            DirectBook_No = null,
-                            DirectBook_Date = DateTime.Today,
-                            Nots = null,
-                            EntryDate = DateTime.Today,
-                            LastUpdateDate = DateTime.Today,
-                            DataEntryName = null,
-                            Image = null,
-                            PDF = null,
-                            PersonalId=1
+                //if (!context.Employment_info.Any())
+                //{
+                //    context.Employment_info.AddRange(new List<Employment_info>()
+                //    {
+                //        new Employment_info()
+                //        {
+                //            FullName = "مستشار",
+                //            MainDeptartment = null,
+                //            SubDeptartment = null,
+                //            Salary = null,
+                //            Service_Continuation = Service_Continuation.continuous,
+                //            Job_Title = null,
+                //            Job_Status = null,
+                //            EmploymentBook_No = null,
+                //            EmploymentBook_Date = DateTime.Today,
+                //            DirectBook_No = null,
+                //            DirectBook_Date = DateTime.Today,
+                //            Nots = null,
+                //            EntryDate = DateTime.Today,
+                //            LastUpdateDate = DateTime.Today,
+                //            DataEntryName = null,
+                //            Image = null,
+                //            PDF = null,
+                //            PersonalId=1
 
-                         },
-                     });
-                    context.SaveChanges();
-                }
+                //         },
+                //     });
+                //    context.SaveChanges();
+                //}
 
                 //Administrative_Orders
-                if (!context.Administrative_Orders.Any())
-                {
-                    context.Administrative_Orders.AddRange(new List<Administrative_Orders>()
-                    {
-                        new Administrative_Orders()
-                        {
-                           // Admi_id = 1,
-                           AdministrativeNo = 0,
-                           OrderNo = 0,
-                           Administrative_Order_Date = DateTime.Today,
-                           Type_Of_Order =null,
-                           Subject=    null,
-                           Summary =null,
-                           Nots = null,
-                           EntryDate = DateTime.Today,
-                           LadtUpdateDate = DateTime.Today,
-                           DataEntryName = null,
-                           Image = null,
-                           PDF = null,
-                           PersonalId= 1,
-                           Employee_id=1
-                         },
+            //    if (!context.Administrative_Orders.Any())
+            //    {
+            //        context.Administrative_Orders.AddRange(new List<Administrative_Orders>()
+            //        {
+            //            new Administrative_Orders()
+            //            {
+            //               // Admi_id = 1,
+            //               AdministrativeNo = 0,
+            //               OrderNo = 0,
+            //               Administrative_Order_Date = DateTime.Today,
+            //               Type_Of_Order =null,
+            //               Subject=    null,
+            //               Summary =null,
+            //               Nots = null,
+            //               EntryDate = DateTime.Today,
+            //               LadtUpdateDate = DateTime.Today,
+            //               DataEntryName = null,
+            //               Image = null,
+            //               PDF = null,
+            //               PersonalId= 1,
+            //               Employee_id=1
+            //             },
 
-                     });
-                    context.SaveChanges();
-                }
+            //         });
+            //        context.SaveChanges();
+            //    }
             }
         }
     }
